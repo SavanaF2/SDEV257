8f0f0cbd5a085275d0e50a2d70e946ddd7d0b4ed
Savana Fallen <113266360+SavanaF2@users.noreply.github.com>
2024-10-29 21:34:35 -0400

    Update App.js

--- my-project/App.js
+++ my-project/App.js
@@ -10,7 +10,7 @@ export default function App() {
 
     <Text style={styles.text}>
 
-    Open up App.js to start working on your app!
+    Hi, my name is Savana, and I am a mobile developer
 
     </Text>
 
