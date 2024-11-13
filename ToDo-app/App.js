f59f2caadba0cd3e39979e6eef66b2c0e4f4511b
Savana Fallen <113266360+SavanaF2@users.noreply.github.com>
2024-11-12 22:44:14 -0500

    Update App.js

--- ToDo-app/App.js
+++ ToDo-app/App.js
@@ -1,20 +1,58 @@
-import React from 'react';
-import { Text, SafeAreaView, StyleSheet } from 'react-native';
+
+import React, {useState} from 'react';
+import { Text, SafeAreaView, StyleSheet, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native';
 import Task from './components/Task';
 
 
 export default function App() {
+const [task, setTask] = useState();
+const [taskItems, setTaskItems] = useState([]);
+
+const handleAddTask = () => {
+  setTaskItems([...taskItems, task])
+  setTask(null);
+}
+const completeTask = (index) =>{
+  let itemsCopy = [...taskItems];
+  itemsCopy.splice(index, 1);
+  setTaskItems(itemsCopy);
+}
   return (
     <SafeAreaView style={styles.container}>
       {/*Today's Tasks*/}
       <SafeAreaView style={styles.tasksWrapper}>
         <Text styles={styles.sectionTitle}> Todays tasks </Text>
         <SafeAreaView styles={styles.items}>
+
           {/*This is where the tasks will go!*/}
-          <Task text={'Do dishes'}/>
-          <Task text={'Finish homework'}/>
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Task text={item} />
+                </TouchableOpacity>
+              );
+              
+            })
+          }
+         
         </SafeAreaView>
       </SafeAreaView>
+
+      {/* Write a task */}
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+        >
+        <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)}/>
+
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <SafeAreaView style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </SafeAreaView>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
@@ -35,5 +73,34 @@ const styles = StyleSheet.create({
  items: {
    marginTop: 30,
  },
+ writeTaskWrapper: {
+   position: 'absolute',
+   bottom: 60,
+   width: '100%',
+   flexDirection: 'row',
+   justifyContent: 'space-between',
+   alignItems: 'center',
+
+ },
+ input:{
+   paddingVertical: 15,
+   paddingHorizontal: 15,
+   backgroundColor:'#FFF',
+   borderRadius: 60,
+   borderColor: '#C0C0C0',
+   borderWidth: 1,
+   width: 250,
+ },
+ addWrapper: {
+   width: 60,
+   heigth: 60,
+   backgroundColor: '#FFF',
+   borderRadius: 60,
+   justifyContent: 'center',
+   alignItems: 'center',
+   borderColor: '#C0C0C0',
+   borderWidth:1,
+ },
+ addText:{},
 
 });
