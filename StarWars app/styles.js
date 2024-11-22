fe3c78f53405fbd5896b9cf18d83c5217efe682c
Savana Fallen <113266360+SavanaF2@users.noreply.github.com>
2024-11-22 01:07:51 -0500

    Update styles.js

---
+++ StarWars app/styles.js
@@ -0,0 +1,25 @@
+  import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  textInputContainer: {
+    alignSelf: "stretch",
+    marginBottom: 30,
+  },
+
+  textInputLabel: {
+    marginBottom: 4,
+  },
+
+ textInput: {
+    backgroundColor: "white",
+    height: 20,
+    fontSize: 11,
+    width:200,
+    borderRadius:4,
+    margin: "auto",
+  },
+  textOutput: {
+    margin: "auto",
+  },
+  
+});
