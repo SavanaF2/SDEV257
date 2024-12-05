e2e45627a41bfa186361ffcabfec6b8d13510f0f
Savana Fallen <113266360+SavanaF2@users.noreply.github.com>
2024-12-04 19:58:31 -0500

    Update Home.js

--- StarWars app/Home.js
+++ StarWars app/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
 import styles from "./styles";
+import Searchbar from "./search";
 export default function Home() {
   return (
-  <View style={styles.container}>
+  <View >
     <Text>Home Content</Text>
+    <Searchbar> </Searchbar>
   </View>
   );
 }
