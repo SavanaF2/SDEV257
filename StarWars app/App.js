426baedb6d3d726cd1d99de4b527f4d7e99dab78
Savana Fallen <113266360+SavanaF2@users.noreply.github.com>
2024-12-12 13:58:29 -0500

    Update App.js

--- StarWars app/App.js
+++ StarWars app/App.js
@@ -4,6 +4,7 @@ import { createDrawerNavigator } from
   "@react-navigation/drawer";
 import { createBottomTabNavigator } from 
   "@react-navigation/bottom-tabs";
+import {createStackNavigator} from '@react-navigation/stack';
 import { Platform } from "react-native";
 import Home from "./Home";
 import Planets from "./Planets";
@@ -11,6 +12,8 @@ import Films from "./Films";
 import Spaceships from "./Spaceships";
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
@@ -20,7 +23,6 @@ export default function App() {
         <Tab.Screen name="Planets" component={Planets}/>
         <Tab.Screen name="Films" component={Films}/>
         <Tab.Screen name="Spaceships" component={Spaceships} />
-
         </Tab.Navigator>
       )}
       {Platform.OS == "android" && (
@@ -28,11 +30,14 @@ export default function App() {
           <Drawer.Screen name="Home" component={Home} />
           <Drawer.Screen name="Planets" component={Planets} />
           <Drawer.Screen name="Films" component={Films} />
-          <Drawer.Screen name="Spaceships" component={Spaceships} />
-            
+          <Drawer.Screen name="Spaceships" component={Spaceships} />  
         </Drawer.Navigator>
       )}
+
+     <Stack.Screen name = "Home" component={Home} />
+     <Stack.Screen name = "Planets" component={Planets} />
+     <Stack.Screen name = "Films" component={Films} />
+      <Stack.Screen name = "Spaceships" component={Spaceships} />
     </NavigationContainer>
   );
 }
-
